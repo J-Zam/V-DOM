161311bf86c149219ad16eb8464d945795dd268b
unknown <zambranojunnior069@gmail.com>
2023-03-30 16:43:15 -0600

    feat: rendering according to the node type

---
+++ src/vdom/render.ts
@@ -0,0 +1,22 @@
+import { IElementNode } from "../ts";
+
+function renderElement({ tagName, attrs, children}: IElementNode) {
+    const $element = document.createElement(tagName)
+
+    for (const [k, v] of Object.entries(attrs)) {
+        $element.setAttribute(k,v)
+    }
+
+    for (const child of children!) {
+        $element.appendChild(render(child))
+    }
+
+    return $element
+}
+
+export function render(vNode: IElementNode) {
+    if (typeof vNode === "string")  
+        return document.createTextNode(vNode)
+
+    return renderElement(vNode)
+}
